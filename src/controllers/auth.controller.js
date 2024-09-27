d5d346c27487ccb750ab644f3f629b156adede41
scino11 <prod.scino@gmail.com>
2024-09-27 18:43:14 -0300

    cookies + rutas protegidas

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -56,12 +56,19 @@ export const loginUser = async (req, res) => {
         }
 
         const token = jwt.sign({ 
-            id: user.id, email: user.email }, 
+            id: user.id, email: user.email, rol:user.rol }, 
             JWT_SECRET, 
             {
                 expiresIn: '24h'
             })
-        
+
+        res.cookie('authToken', token, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production',
+            sameSite: 'Strict',
+            maxAge: 24 * 60 * 60 * 1000
+        })
+
         res.status(200).json({ 
             message: 'Inicio de sesión exitoso', 
             token,
--- src/middlewares/cookies.middleware.js
+++ src/middlewares/cookies.middleware.js
@@ -1,12 +1,13 @@
 import jwt from 'jsonwebtoken';
 import { JWT_SECRET } from '../config/dotenv.config.js';
 
-export const verifyToken = (req, res, next) => {
-    const token = req.headers['authorization']
+export const verifyTokenCookie = (req, res, next) => {
+    const token = req.cookies.authToken
 
     if(!token) {
         return res.status(403).json({ message: 'Token no proporcionado' });
     }
+
     try {
         const decoded = jwt.verify(token, JWT_SECRET)
         req.user = decoded;
@@ -14,11 +15,4 @@ export const verifyToken = (req, res, next) => {
     } catch (error) {
         return res.status(401).json({ message: 'Token inválido o expirado' });
     }
-}
-
-export const isAdmin = (req, res, next) => {
-    if(req.user.rol !== 'admin') {
-        return res.status(403).json({ message: 'Acceso denegado: no sos administrador' });
-    }
-    next()
 }
