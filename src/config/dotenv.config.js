760f9e59cc69566d7eb8b5c2c890a622b31fde74
scino11 <prod.scino@gmail.com>
2024-10-04 21:29:48 -0300

    config

--- src/config/dotenv.config.js
+++ src/config/dotenv.config.js
@@ -5,8 +5,8 @@ config()
 export const PORT = process.env.PORT || 1234
 export const DB_HOST = process.env.DB_HOST || 'localhost'
 export const DB_USER = process.env.DB_USER || 'root'
-export const DB_PASSWORD = process.env.DB_PASSWORD || '7410056Chino!'
-export const DB_DATABASE = process.env.DB_DATABASE || 'utn_mantenimiento'
+export const DB_PASSWORD = process.env.DB_PASSWORD || 'WMzdIQBXQtiHXtkRIZtUlIPTSHyEVkgM'
+export const DB_DATABASE = process.env.DB_DATABASE || 'railway'
 export const DB_PORT = process.env.DB_PORT || 3306
 
 export const JWT_SECRET = 'X7$h&9pJ3$*Ln3Vf5%WkQ!#RzB8dYxCt7@q6JvP'
